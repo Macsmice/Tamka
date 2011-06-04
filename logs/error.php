329187e1ef4fec4e0998aa5f5d6f71cbee85fa5a
Babs Gösgens <babsgosgens@gmail.com>
2011-06-04 09:21:27 +0200

    Removed config specific items and moved them to git/info/exclude

--- logs/error.php
+++
@@ -1,7 +0,0 @@
-#<?php die('Direct Access To Log Files Not Permitted'); ?>
-#Version: 1.0
-#Date: 2011-05-04 15:13:38
-#Fields: date	time	level	c-ip	status	comment
-#Software: Joomla! 1.6.3 Stable [ Onward ] 18-Apr-2011 23:00 GMT
-2011-05-04	15:13:38	-	127.0.0.1	Joomla FAILURE: 	Invalid password
-2011-05-20	04:09:52	-	::1	Joomla FAILURE: 	Empty password not allowed
